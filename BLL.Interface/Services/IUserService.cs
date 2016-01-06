1cf47533d3338e26a547c5f53d1d74c9f9e24136
v.shienenko <vadim.shienenko@gmail.com>
2016-01-06 17:09:16 +0300

    Add RoleEntity and IUserService to BLL.InterACE

--- BLL.Interface/Services/IUserService.cs
+++ BLL.Interface/Services/IUserService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BLL.Interface.Entities;
 
 namespace BLL.Interface.Services
 {
     public interface IUserService
     {
-        void CreateUser();
+        void CreateUser(UserEntity user);
+        bool DeleteUser(UserEntity user);
     }
 }
