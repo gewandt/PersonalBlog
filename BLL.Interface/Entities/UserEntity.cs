94538f2cc8e8ae7113e0fe8289a7dca822137007
v.shienenko <vadim.shienenko@gmail.com>
2016-01-06 16:45:48 +0300

    Add library BLL and Entity + Service classes

---
+++ BLL.Interface/Entities/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interface.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public int RoleId { get; set; }
+    }
+}
